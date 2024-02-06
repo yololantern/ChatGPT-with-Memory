733abe7ab668a11e8721abc460a1ee1b3408fa2e
SSS <142176901+sss7526@users.noreply.github.com>
2024-02-06 02:08:51 +0000

    removed arb comments

--- GPTwithMemory.py
+++ GPTwithMemory.py
@@ -1,17 +1,17 @@
 from openai import OpenAI
+
+# reads your API key from key.txt
 try:
     with open('key.txt', 'r') as r:
         key = r.read().rstrip('\n')
 except:
     print("Error: You need to put your API key in a file named 'key.txt'.")
     exit()
+
 # Set up your OpenAI API credentials
 client = OpenAI(api_key = key)
 
-# Arb comment line
-
 def chat_with_gpt(messages):
-    # userinput = input('What is your question? ')
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages
@@ -21,17 +21,23 @@ def chat_with_gpt(messages):
     print("\n -+-+-+-+-+- \n")
     return resp
 
-# Start the chat loop
-messages = [{"role": "system", "content": "You can start chatting by saying 'Hello'."}]
+def main():
 
-while True:
-    user_input = input("User: ")
-    messages.append({"role": "user", "content": user_input})
+    # Start the chat loop
+    messages = [{"role": "system", "content": "You can start chatting by saying 'Hello'."}]
+
+    while True:
+        user_input = input("User: ")
+
+        # keeps your prompts in context memory
+        messages.append({"role": "user", "content": user_input})
     
-    messages.append({"role": "system", "content": chat_with_gpt(messages)})
+        # keeps AI response in context memory
+        messages.append({"role": "system", "content": chat_with_gpt(messages)})
 
-    # chat_with_gpt(messages)
+        # Stop the loop if the user says goodbye
+        if user_input.lower() == 'goodbye':
+            break
 
-    # Stop the loop if the user says goodbye
-    if user_input.lower() == 'goodbye':
-        break
+if __name__ == '__main__':
+    main()
